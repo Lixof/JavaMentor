1801db811306c7b88c3f86e3d2393f4f40700587
VIACHESLAV LIKHOV <Lixoffs@gmail.com>
2021-01-25 02:33:11 +0300

    commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -9,19 +9,55 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-        Statement stmt = Util.statement();
+        try {
+            Statement stmt = Util.statement();
+            String sql = "CREATE TABLE REGISTRATION " +
+                    "(id INTEGER AUTO_INCREMENT, " +
+                    " name TINYTEXT, " +
+                    " last_name TINYTEXT, " +
+                    " age TINYINT, " +
+                    " PRIMARY KEY ( id ))";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+        } finally {
+            Util.close();
+        }
     }
 
     public void dropUsersTable() {
-
+        try {
+            Statement stmt = Util.statement();
+            String sql = "DROP TABLE REGISTRATION ";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+        } finally {
+            Util.close();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        System.out.println("User с именем - " + name + " добавлен в базу данных");
+        try {
+            Statement stmt = Util.statement();
+            String sql = "INSERT REGISTRATION(name, last_name, age) " +
+                    "VALUES ('" + name + "', '" + lastName + "', " + age + ") ";
+            stmt.executeUpdate(sql);
+            System.out.println("User с именем - " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+        } finally {
+            Util.close();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try {
+            Statement stmt = Util.statement();
+            String sql = "DELETE FROM REGISTRATION " +
+                    "WHERE id =" + id + " " ;
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+        } finally {
+            Util.close();
+        }
     }
 
     public List<User> getAllUsers() {
@@ -29,6 +65,13 @@ public class UserServiceImpl implements UserService {
     }
 
     public void cleanUsersTable() {
-
+        try {
+            Statement stmt = Util.statement();
+            String sql = "TRUNCATE TABLE  REGISTRATION ";
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+        } finally {
+            Util.close();
+        }
     }
 }
