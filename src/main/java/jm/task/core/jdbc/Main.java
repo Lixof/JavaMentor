6af491e827ece31a8c025e5bd0f45ebc2d6a1dcc
VIACHESLAV LIKHOV <Lixoffs@gmail.com>
2021-01-24 23:43:54 +0300

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,8 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.*;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService usi = new UserServiceImpl();
+        usi.createUsersTable();
+        User first = new User("Аня","Караулан",(byte) 20);
+        User second = new User("Гриша","Капустин",(byte) 22);
+        User third = new User("Лиза","Лихова",(byte) 14);
+        User fourth = new User("Евгений","Лихов",(byte) 47);
+        usi.saveUser(first.getName(), first.getLastName(), first.getAge());
+        usi.saveUser(second.getName(), second.getLastName(), second.getAge());
+        usi.saveUser(third.getName(), third.getLastName(), third.getAge());
+        usi.saveUser(fourth.getName(), fourth.getLastName(), fourth.getAge());
+        List<User> users = usi.getAllUsers();
+        for (User user : users) System.out.println(user.toString());
+        usi.cleanUsersTable();
+        usi.dropUsersTable();
     }
 }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,8 @@
 package jm.task.core.jdbc.util;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    String url = ("jdbc:mysql://localhost:3306/mydbtest");
+    String username = ("root");
+    String password = ("root");
+
 }
