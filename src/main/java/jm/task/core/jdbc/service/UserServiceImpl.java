357985823d3a0685fc75ae7f6288965365235424
VIACHESLAV LIKHOV <Lixoffs@gmail.com>
2021-01-25 01:19:49 +0300

    commit

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-
+        Statement stmt = Util.statement();
     }
 
     public void dropUsersTable() {
@@ -14,7 +17,7 @@ public class UserServiceImpl implements UserService {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        System.out.println("User с именем - " + name + " добавлен в базу данных");
     }
 
     public void removeUserById(long id) {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,37 @@
 package jm.task.core.jdbc.util;
 
-public class Util {
-    String url = ("jdbc:mysql://localhost:3306/mydbtest");
-    String username = ("root");
-    String password = ("root");
+import java.sql.* ;
 
+import java.lang.* ;
+
+public class Util {
+    private final static String url = ("jdbc:mysql://localhost:3306/mydbtest");
+    private final static String username = ("root");
+    private final static String password = ("root");
+    private static Connection connection;
+    private static Statement statement;
+    public static Statement statement() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(url, username, password);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            e.getStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.getStackTrace();
+        }
+        return statement;
+    }
+    public static void close() {
+        try {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+    }
 }
