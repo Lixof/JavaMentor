ed8216e07391d445b398b169c26c7f563d140d74
VIACHESLAV LIKHOV <Lixoffs@gmail.com>
2021-02-08 00:38:33 +0300

    commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -18,88 +18,76 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-        try {
-            String sql = "CREATE TABLE registration " +
+        String sql = "CREATE TABLE IF NOT EXISTS registration " +
                     "(id INTEGER AUTO_INCREMENT, " +
                     " name TINYTEXT, " +
                     " lastName TINYTEXT, " +
                     " age TINYINT, " +
                     " PRIMARY KEY ( id ))";
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = null;
-            transaction = session.beginTransaction();
-            session.createSQLQuery(sql).executeUpdate();
-            transaction.commit();
-            session.close();
-        } catch (SQLGrammarException e) {}
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = null;
+        transaction = session.beginTransaction();
+        session.createSQLQuery(sql).executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void dropUsersTable() {
-        try {
-            String sql = "DROP TABLE registration ";
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = null;
-            transaction = session.beginTransaction();
-            session.createSQLQuery(sql).executeUpdate();
-            transaction.commit();
-            session.close();
-        } catch (SQLGrammarException e) {}
+        String sql = "DROP TABLE IF EXISTS registration ";
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = null;
+        transaction = session.beginTransaction();
+        session.createSQLQuery(sql).executeUpdate();
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try {
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = null;
-            transaction = session.beginTransaction();
-            User user = new User(name, lastName, age);
-            user.setId(null);
-            session.save(user);
-            System.out.println("User с именем - " + name + " добавлен в бвзу данных");
-            transaction.commit();
-            session.close();
-        } catch (SQLGrammarException e) {}
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = null;
+        transaction = session.beginTransaction();
+        User user = new User(name, lastName, age);
+        user.setId(null);
+        session.save(user);
+        System.out.println("User с именем - " + name + " добавлен в бвзу данных");
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public void removeUserById(long id) {
-        try {
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = null;
-            transaction = session.beginTransaction();
-            User user = (User) session.get(User.class, id);
-            session.delete(user);
-            transaction.commit();
-            session.close();
-        } catch (SQLGrammarException e) {}
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = null;
+        transaction = session.beginTransaction();
+        User user = (User) session.get(User.class, id);
+        session.delete(user);
+        transaction.commit();
+        session.close();
     }
 
     @Override
     public List<User> getAllUsers() {
-        try {
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = null;
-            transaction = session.beginTransaction();
-            List<User> users = (List<User>) session.createQuery("FROM User").list();
-            transaction.commit();
-            session.close();
-            return users;
-        } catch (SQLGrammarException e) {return null;}
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = null;
+        transaction = session.beginTransaction();
+        List<User> users = (List<User>) session.createQuery("FROM User").list();
+        transaction.commit();
+        session.close();
+        return users;
     }
 
     @Override
     public void cleanUsersTable() {
-        try {
-            Session session = Util.getSessionFactory().openSession();
-            Transaction transaction = null;
-            transaction = session.beginTransaction();
-            List<User> users = (List<User>) session.createQuery("FROM User").list();
-            for (User user : users){
-                session.delete(user);
-            }
-            transaction.commit();
-            session.close();
-        } catch (SQLGrammarException e) {}
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = null;
+        transaction = session.beginTransaction();
+        List<User> users = (List<User>) session.createQuery("FROM User").list();
+        for (User user : users){
+            session.delete(user);
+        }
+        transaction.commit();
+        session.close();
     }
 }
